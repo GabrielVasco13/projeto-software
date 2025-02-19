e079c23dddcd305a20555305417f9eee89c6faa2
Gabriel <gabrielvasco906@gmail.com>
2025-02-19 10:09:46 -0300

    primeiro commit

---
+++ src/temp.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class temp {
+
+	public static void main(String[] args) {
+		Scanner entry = new Scanner(System.in);
+		
+		System.out.println("Digite a temperatura em graus celsius: ");
+		double celsius = entry.nextDouble();
+		
+		double fahrenheit = (celsius * 9/5) + 32;
+		double kelvin = celsius + 273.15;
+		
+		System.out.println("A temperatura em fahrenheit é: " + fahrenheit);
+		System.out.println("A temperatura em kelvin é: " + kelvin);
+		
+		entry.close();
+
+	}
+
+}
