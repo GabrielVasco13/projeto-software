df1156a896953f545d6333f64d7f4e0987325dfc
GabrielVasco13 <gabrielvasco906@gmail.com>
2025-03-12 10:34:27 -0300

    dps fazer a tabuada

---
+++ loops/src/For.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class For {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite um número: ");
+		int numero = sc.nextInt();
+		
+		for (int i = 0; i <=10; i++) {
+			System.out.println("Resulado" + i);
+		}
+
+	}
+
+}
